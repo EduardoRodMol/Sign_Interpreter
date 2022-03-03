e2d6431a4b262eaf935016261b8388741d7d34ca
BorjaEACode <espesborja@gmail.com>
2022-03-03 13:53:21 +0100

    arreglo codigo

--- SignInterpreter/data/keypoints.py
+++ SignInterpreter/data/keypoints.py
@@ -13,22 +13,10 @@ def mediapipe_detection(image, model):
     return image, results
     
 def draw_landmarks(image, results):
-    #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS) # Draw face connections
-    #mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections    
 
 def draw_styled_landmarks(image, results):
-    # Draw face connections
-    #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
-                             #mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                             #mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             #) 
-    # Draw pose connections
-    #mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                             #mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                             #mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                             #) 
     # Draw left hand connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
                              mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
