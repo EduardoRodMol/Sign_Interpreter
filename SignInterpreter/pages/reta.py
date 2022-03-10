b0b36a038786fc631e8e14ab0122651803c7103b
unknown <eduardo.rodriguez.molina@hotmail.es>
2022-03-10 12:26:15 +0100

    orden

--- SignInterpreter/pages/reta.py
+++
@@ -1,6 +0,0 @@
-from juego.game import juego, gesto, selecciona_random
-
-for i in range (10):
-    predict =  selecciona_random(gesto)
-    print(juego (predict))
-   
--- SignInterpreter/pronosticador/predict.py
+++ SignInterpreter/pronosticador/predict.py
@@ -6,6 +6,10 @@ from data.datarecord import actions
 import numpy as np
 import cv2
 
+global model 
+model = load_model("action.h5")
+
+
 def extraerkeypoints(img,holistic):    
     
     image, results = mediapipe_detection(img, holistic)
@@ -14,19 +18,19 @@ def extraerkeypoints(img,holistic):
     return keypoints
 
 
-
 def update_cv2(img,label):
         cv2.rectangle(img, (0,0), (640, 40), (245, 117, 16), -1)
         cv2.putText(img, ' '.join(label), (3,30), 
-                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-def predice(sequence,holistic):
-    model = load_model("action.h5")
+                       cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+
+def predice(sequence):
+    global model
     #sequence=[]
     sentence =[]
     threshold = 0.8
     res = model.predict(np.expand_dims(sequence, axis=0))[0]
-    label = actions[np.argmax(res)]
-        
+    #label = actions[np.argmax(res)]       
         
     if res[np.argmax(res)] > threshold: 
             if len(sentence) > 0: 
@@ -38,4 +42,4 @@ def predice(sequence,holistic):
     if len(sentence) > 5: 
         sentence = sentence[-5:]
 
-    return sentence
+    return str(sentence[0])
